3ceea1cb5e8dce32fd8c1c3de02e317b7ca1eb01
twai <pikachu2015@naver.com>
2026-04-04 18:02:13 +0900

    Add files via upload

---
+++ 260404.cpp
@@ -0,0 +1,215 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int A, B, C;
+//	scanf("%d %d %d", &A, &B, &C);
+//	printf("%d\n",(A + B) % C);
+//	printf("%d\n",((A % C)+(B % C)) % C);
+//	printf("%d\n",(A * B) % C);
+//	printf("%d\n", ((A % C) * (B % C)) % C);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int A, B, C;
+//	scanf("%d %d %d", &A, &B, &C);
+//	printf("%d", A + B + C);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\\    /\\ \n )  ( ')\n(  /  )\n\\(__)|");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 6;
+//
+//	while (count > 0)
+//	{
+//		printf("C\n");
+//		count--;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = -10;
+//
+//	while (count <= 0)
+//	{
+//		printf("%d\n", count);
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 0;
+//
+//	while (count <= 100)
+//	{
+//		printf("%d\n", count);
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	int first = a / 100;
+//	a = a % 100;
+//	printf("first:%d, a:%d\n", first, a);
+//	int second = a / 10;
+//	a = a % 10;
+//	printf("second:%d, a:%d\n", second, a);
+//	int third = a / 1;
+//	a = a % 1;
+//	printf("third:%d, a:%d\n", third, a);
+//	int wfirst = b / 100;
+//	b = b % 100;
+//	printf("wfirst:%d, a:%d\n", wfirst, b);
+//	int wsecond = b / 10;
+//	b = b % 10;
+//	printf("wsecond:%d, a:%d\n", wsecond, b);
+//	int wthird = b / 1;
+//	b = b % 1;
+//	printf("wthird:%d, a:%d\n", wthird, b);
+//	int ans1, ans2, ans3;
+//	ans1 = (first * 100)* wthird + (second * 10) * wthird + third * wthird;
+//	ans2 = (first * 100) * wsecond + (second * 10) * wsecond + third * wsecond;
+//	ans3 = (first * 100) * wfirst + (second * 10) * wfirst + third * wfirst;
+//	printf("%d\n", ans1);
+//	printf("%d\n", ans2);
+//	printf("%d\n", ans3);
+//	printf("%d\n", ans3 * 100 + ans2 * 10 + ans1);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int A, B, C;
+//	scanf("%d %d %d", &A, &B, &C);
+//	printf("%d\n",(A + B) % C);
+//	printf("%d\n",((A % C)+(B % C)) % C);
+//	printf("%d\n",(A * B) % C);
+//	printf("%d\n", ((A % C) * (B % C)) % C);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int A, B, C;
+//	scanf("%d %d %d", &A, &B, &C);
+//	printf("%d", A + B + C);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\\    /\\ \n )  ( ')\n(  /  )\n\\(__)|");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 6;
+//
+//	while (count > 0)
+//	{
+//		printf("C\n");
+//		count--;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = -10;
+//
+//	while (count <= 0)
+//	{
+//		printf("%d\n", count);
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int count = 0;
+//
+//	while (count <= 100)
+//	{
+//		printf("%d\n", count);
+//		count++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	int first = a / 100;
+//	a = a % 100;
+//	int second = a / 10;
+//	a = a % 10;
+//	int third = a / 1;
+//	a = a % 1;
+//	int wfirst = b / 100;
+//	b = b % 100;
+//	int wsecond = b / 10;
+//	b = b % 10;
+//	int wthird = b / 1;
+//	b = b % 1;
+//	int ans1, ans2, ans3;
+//	ans1 = (first * 100) * wthird + (second * 10) * wthird + third * wthird;
+//	ans2 = (first * 100) * wsecond + (second * 10) * wsecond + third * wsecond;
+//	ans3 = (first * 100) * wfirst + (second * 10) * wfirst + third * wfirst;
+//	printf("%d\n", ans1);
+//	printf("%d\n", ans2);
+//	printf("%d\n", ans3);
+//	printf("%d\n", ans3 * 100 + ans2 * 10 + ans1);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	long long int A, B, C;
+//	scanf("%lld %lld %lld", &A, &B, &C);
+//	printf("%lld", A + B + C);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\\    /\\ \n )  ( ')\n(  /  )\n \\(__)|");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("|\\_/|\n");
+//	printf("|q p|   /\}\n");
+//	printf("( 0 )\"\"\"\\\n");
+//	printf("|\"\^\"\`    |\n");
+//	printf("|| _ /= \\\\__ | ");
+//}
